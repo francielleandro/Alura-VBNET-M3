54ff77c1a3fe3eb41492acc1f1e9aba12a40973d
Franciel Leandro <francielleandro@gmail.com>
2021-02-23 04:06:30 -0300

    commit inicial

--- ByteBank/Form1.vb
+++ ByteBank/Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form1
+    Public Sub New()
 
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Lbl_Principal.Text = "Principal"
+    End Sub
 End Class
