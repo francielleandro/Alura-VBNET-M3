676a54e110e130d30ce4c55b079baea32c74067f
Franciel Leandro <francielleandro@gmail.com>
2021-02-23 04:13:26 -0300

    estruturando o projeto

---
+++ ByteBank/Classes/Funcionarios/Funcionario.vb
@@ -0,0 +1,10 @@
+﻿Public Class Funcionario
+    Public Property nome As String
+    Public Property cpf As String
+    Public Property salario As Double
+
+
+    Public Function GetBonificacao() As Double
+        Return salario * 0.1
+    End Function
+End Class
