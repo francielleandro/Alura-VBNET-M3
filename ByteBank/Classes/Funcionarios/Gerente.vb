1c7c8a650e98fc92b8937f41e555e1622812bc24
Franciel Leandro <francielleandro@gmail.com>
2021-02-23 04:37:03 -0300

    Criando classes de funcionarios

--- ByteBank/Classes/Funcionarios/Gerente.vb
+++ ByteBank/Classes/Funcionarios/Gerente.vb
@@ -1,34 +1,21 @@
 ﻿Namespace Classes.Funcionarios
-
-    Public Class Funcionario
-
+    Public Class Gerente
 #Region "Property"
 
         Public Property nome As String
         Public Property cpf As String
         Public Property salario As Double
-        Public Property cargo As String
 
 #End Region
 
 #Region "Constructos"
-        Public Sub New(_cargo As String)
-            cargo = _cargo
-        End Sub
+
 #End Region
 
 #Region "Functions"
         Public Function GetBonificacao() As Double
-            If cargo = "1" Then
-                Return salario * 0.1
-            ElseIf cargo = "2" Then
-                Return salario * 1
-            End If
-            Return 0
+            Return salario * 0.1
         End Function
 #End Region
-
     End Class
 End Namespace
-
-
--- ByteBank/Forms/Frm_Principal.vb
+++ ByteBank/Forms/Frm_Principal.vb
@@ -13,13 +13,21 @@ Public Class Frm_Principal
     End Sub
 
     Private Sub Btn_Click_Click(sender As Object, e As EventArgs) Handles Btn_Click.Click
-        Dim Carlos As New Funcionario
+        Dim Carlos As New Diretor()
         Carlos.nome = "Carlos"
         Carlos.cpf = "524.445.434-00"
         Carlos.salario = 2000
 
+        Dim Bia As New Gerente()
+        Bia.nome = "Bia"
+        Bia.cpf = "524.445.414-01"
+        Bia.salario = 2000
+
         MsgBox("O sálario do " + Carlos.nome + " é " + Carlos.salario.ToString)
         MsgBox("A bonificação de " + Carlos.nome + " é " + Carlos.GetBonificacao.ToString)
+
+        MsgBox("O sálario do " + Bia.nome + " é " + Bia.salario.ToString)
+        MsgBox("A bonificação de " + Bia.nome + " é " + Bia.GetBonificacao.ToString)
     End Sub
 
 End Class
